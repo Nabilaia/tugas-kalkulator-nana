82e07ae53f6ee72b4f038cc9be60da082350e6c3
Nabilaia <70675989+Nabilaia@users.noreply.github.com>
2020-10-01 03:19:20 -0700

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,26 @@
+var tampil = document.formAngka.isiText;
+function ins(num) {
+ tampil.value += num;
+}
+function samaDengan() {
+ tampil.value = eval(tampil.value);
+}
+
+function DELETE() {
+ tampil.value = "";
+}
+function ins num(){
+	tampil.value -= num;
+}
+function ins num(){
+	tampil.value /= num;
+}
+function ins num (){
+	tampil.value %= num;
+}
+function ins num (){
+	tampil.value *= num;
+}
+function ins value (){
+	tampil.value 
+}
